21b492b1e95c5a93e1d3b81d2b6fdff5b6e5ba42
happyrhkr <happyrhkr@g.hongik.ac.kr>
2023-08-02 00:40:09 +0900

    add 3D style component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Story3D from "../components/Story3D";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Story3D />
       <Footer />
     </div>
   );
