f24c11b4d1d22c927f7635e6cbcdac5326894d77
happyrhkr <happyrhkr@g.hongik.ac.kr>
2023-08-07 16:21:13 +0900

    add Star

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Story3D from "../components/Story3D";
+import Star from "../components/Star";
+import StarRating from "../components/StarRating";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <Main />
       <Story3D />
       <Footer />
+      <Star />
+      <StarRating />
     </div>
   );
 };
